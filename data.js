5ea9b5769fa40a06a56ff5c098b8c94e9b066c81
ㅇㅇ <woawoa75@naver.com>
2025-11-18 19:47:14 +0900

    css 및 data 등록

---
+++ data.js
@@ -0,0 +1,542 @@
+export const projects = [
+    {
+        name: "css_3D button",
+        url: "img/css_3D button.png",
+        tag: ["button", "css"],
+    },
+    {
+        name: "css_button모음",
+        url: "img/css_button모음.png",
+        tag: ["button", "css"],
+    },
+    {
+        name: "css_card--01",
+        url: "img/css_card--01.png",
+        tag: ["card", "icon", "css"],
+    },
+    {
+        name: "css_card--02",
+        url: "img/css_card--02.png",
+        tag: ["card", "icon", "css"],
+    },
+    {
+        name: "css_dropdown nav ani",
+        url: "img/css_dropdown nav ani.png",
+        tag: ["nav", "dropdown", "menu", "css"],
+    },
+    {
+        name: "css_Fullscreen menu",
+        url: "img/css_Fullscreen menu.png",
+        tag: ["nav", "fullscreen", "menu", "css"],
+    },
+    {
+        name: "css_hover_img고정",
+        url: "img/css_hover_img고정.png",
+        tag: ["hover", "img", "css"],
+    },
+    {
+        name: "css_img_hover--03",
+        url: "img/css_img_hover--03.png",
+        tag: ["card", "hover", "img", "css"],
+    },
+    {
+        name: "css_mouseDown",
+        url: "img/css_mouseDown.png",
+        tag: ["css"],
+    },
+    {
+        name: "css_pulse",
+        url: "img/css_pulse.png",
+        tag: ["css"],
+    },
+    {
+        name: "css_sliding_card",
+        url: "img/css_sliding_card.png",
+        tag: ["css", "4칸"],
+    },
+    {
+        name: "css_text hover",
+        url: "img/css_text hover.png",
+        tag: ["text", "css", "hover"],
+    },
+    {
+        name: "css_text 반사",
+        url: "img/css_text 반사.png",
+        tag: ["text", "css"],
+    },
+    {
+        name: "css_text_reveal_animation",
+        url: "img/css_text_reveal_animation.png",
+        tag: ["reveal", "css", "landing"],
+    },
+    {
+        name: "css_txt가운데갈라짐",
+        url: "img/css_txt가운데갈라짐.png",
+        tag: ["text", "css", "hover"],
+    },
+    {
+        name: "css_txt스캐닝",
+        url: "img/css_txt스캐닝.png",
+        tag: ["text", "css", "hover"],
+    },
+    {
+        name: "css_늘리는버튼",
+        url: "img/css_늘리는버튼.png",
+        tag: ["button", "css", "hover"],
+    },
+    {
+        name: "css_안쪽으로 파인 라운드",
+        url: "img/css_안쪽으로 파인 라운드.png",
+        tag: ["card", "css"],
+    },
+    {
+        name: "css_좌우로 벌어지는",
+        url: "img/css_좌우로 벌어지는.png",
+        tag: ["css", "4칸"],
+    },
+    {
+        name: "css_화살표_button",
+        url: "img/css_화살표_button.png",
+        tag: ["button"],
+    },
+    {
+        name: "js_4가지이미지펄쳐짐",
+        url: "img/js_4가지이미지펄쳐짐.png",
+        tag: ["4칸"],
+    },
+    {
+        name: "js_border mouse",
+        url: "img/js_border mouse.png",
+        tag: ["mouse"],
+    },
+    {
+        name: "js_Circular Navigation Menu",
+        url: "img/js_Circular Navigation Menu.png",
+        tag: ["button", "floating"],
+    },
+    {
+        name: "js_circular_progress_bar",
+        url: "img/js_circular_progress_bar.png",
+        tag: ["progress"],
+    },
+    {
+        name: "js_grained 노이즈 배경",
+        url: "img/js_grained 노이즈 배경.png",
+        tag: ["background", "노이즈"],
+    },
+    {
+        name: "js_gsap_CustomEase--01",
+        url: "img/js_gsap_CustomEase--01.png",
+        tag: ["gsap", "landing"],
+    },
+    {
+        name: "js_gsap_CustomEase--02",
+        url: "img/js_gsap_CustomEase--02.png",
+        tag: ["gsap", "landing", "countdown"],
+    },
+    {
+        name: "js_gsap_flip--02",
+        url: "img/js_gsap_flip--02.png",
+        tag: ["gsap", "card"],
+    },
+    {
+        name: "js_gsap_flip--03",
+        url: "img/js_gsap_flip--03.png",
+        tag: ["gsap", "text"],
+    },
+    {
+        name: "js_gsap_flip--04",
+        url: "img/js_gsap_flip--04.png",
+        tag: ["gsap", "card"],
+    },
+    {
+        name: "js_gsap_flip--05",
+        url: "img/js_gsap_flip--05.png",
+        tag: ["gsap", "card"],
+    },
+    {
+        name: "js_gsap_flip--06",
+        url: "img/js_gsap_flip--06.png",
+        tag: ["gsap", "img"],
+    },
+    {
+        name: "js_gsap_flip--07",
+        url: "img/js_gsap_flip--07.png",
+        tag: ["gsap", "img", "circular"],
+    },
+    {
+        name: "js_gsap_hoverimg--08",
+        url: "img/js_gsap_hoverimg--08.png",
+        tag: ["gsap", "hover", "pixel"],
+    },
+    {
+        name: "js_gsap_hoverimg--09",
+        url: "img/js_gsap_hoverimg--09.png",
+        tag: ["gsap", "hover", "img"],
+    },
+    {
+        name: "js_gsap_landing--07",
+        url: "img/js_gsap_landing--07.png",
+        tag: ["gsap", "landing", "mouse"],
+    },
+    {
+        name: "js_gsap_landing--09",
+        url: "img/js_gsap_landing--09.png",
+        tag: ["gsap", "landing"],
+    },
+    {
+        name: "js_gsap_menu--01",
+        url: "img/js_gsap_menu--01.png",
+        tag: ["gsap", "menu", "fullscreen"],
+    },
+    {
+        name: "js_gsap_menu--02",
+        url: "img/js_gsap_menu--02.png",
+        tag: ["gsap", "menu", "fullscreen"],
+    },
+    {
+        name: "js_gsap_menu--08",
+        url: "img/js_gsap_menu--08.png",
+        tag: ["gsap", "menu", "fullscreen"],
+    },
+    {
+        name: "js_gsap_menu--09",
+        url: "img/js_gsap_menu--09.png",
+        tag: ["gsap", "menu", "fullscreen"],
+    },
+    {
+        name: "js_gsap_ScrollSmoother--01",
+        url: "img/js_gsap_ScrollSmoother--01.png",
+        tag: ["gsap", "스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollSmoother--02",
+        url: "img/js_gsap_ScrollSmoother--02.png",
+        tag: ["gsap", "가로스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollSmoother--03",
+        url: "img/js_gsap_ScrollSmoother--03.png",
+        tag: ["gsap", "스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollSmoother--04",
+        url: "img/js_gsap_ScrollSmoother--04.png",
+        tag: ["gsap", "스크롤", "galley"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--01",
+        url: "img/js_gsap_ScrollTrigger--01.png",
+        tag: ["gsap", "스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--02",
+        url: "img/js_gsap_ScrollTrigger--02.png",
+        tag: ["gsap", "스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--03",
+        url: "img/js_gsap_ScrollTrigger--03.png",
+        tag: ["gsap", "스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--04-01",
+        url: "img/js_gsap_ScrollTrigger--04-01.png",
+        tag: ["gsap", "가로스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--04-02",
+        url: "img/js_gsap_ScrollTrigger--04-02.png",
+        tag: ["gsap", "가로스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--05",
+        url: "img/js_gsap_ScrollTrigger--05.png",
+        tag: ["gsap", "스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--06",
+        url: "img/js_gsap_ScrollTrigger--06.png",
+        tag: ["gsap", "스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--07",
+        url: "img/js_gsap_ScrollTrigger--07.png",
+        tag: ["gsap", "스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--08",
+        url: "img/js_gsap_ScrollTrigger--08.png",
+        tag: ["gsap", "스크롤", "text"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--09",
+        url: "img/js_gsap_ScrollTrigger--09.png",
+        tag: ["gsap", "스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--10",
+        url: "img/js_gsap_ScrollTrigger--10.png",
+        tag: ["gsap", "스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--11",
+        url: "img/js_gsap_ScrollTrigger--11.png",
+        tag: ["gsap", "스크롤", "galley"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--12",
+        url: "img/js_gsap_ScrollTrigger--12.png",
+        tag: ["gsap", "스크롤", "galley"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--13",
+        url: "img/js_gsap_ScrollTrigger--13.png",
+        tag: ["gsap", "스크롤", "text"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--14",
+        url: "img/js_gsap_ScrollTrigger--14.png",
+        tag: ["gsap", "스크롤", "galley"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--15",
+        url: "img/js_gsap_ScrollTrigger--15.png",
+        tag: ["gsap", "스크롤", "reveal"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--16",
+        url: "img/js_gsap_ScrollTrigger--16.png",
+        tag: ["gsap", "스크롤", "galley"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--17",
+        url: "img/js_gsap_ScrollTrigger--17.png",
+        tag: ["gsap", "스크롤", "galley"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--18",
+        url: "img/js_gsap_ScrollTrigger--18.png",
+        tag: ["gsap", "스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--19",
+        url: "img/js_gsap_ScrollTrigger--19.png",
+        tag: ["gsap", "스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--20",
+        url: "img/js_gsap_ScrollTrigger--20.png",
+        tag: ["gsap", "스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--21",
+        url: "img/js_gsap_ScrollTrigger--21.png",
+        tag: ["gsap", "스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--22",
+        url: "img/js_gsap_ScrollTrigger--22.png",
+        tag: ["gsap", "가로스크롤", "circular"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--23",
+        url: "img/js_gsap_ScrollTrigger--23.png",
+        tag: ["gsap", "가로스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--24",
+        url: "img/js_gsap_ScrollTrigger--24.png",
+        tag: ["gsap", "text"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--25",
+        url: "img/js_gsap_ScrollTrigger--25.png",
+        tag: ["gsap", "text", "스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--26",
+        url: "img/js_gsap_ScrollTrigger--26.png",
+        tag: ["gsap", "스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--27",
+        url: "img/js_gsap_ScrollTrigger--27.png",
+        tag: ["gsap", "스크롤", "img"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--28",
+        url: "img/js_gsap_ScrollTrigger--28.png",
+        tag: ["gsap", "text", "스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--29",
+        url: "img/js_gsap_ScrollTrigger--29.png",
+        tag: ["gsap", "text", "스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--30",
+        url: "img/js_gsap_ScrollTrigger--30.png",
+        tag: ["gsap", "text"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--31",
+        url: "img/js_gsap_ScrollTrigger--31.png",
+        tag: ["gsap", "스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--32",
+        url: "img/js_gsap_ScrollTrigger--32.png",
+        tag: ["gsap", "스크롤", "img"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--33",
+        url: "img/js_gsap_ScrollTrigger--33.png",
+        tag: ["gsap", "스크롤", "background"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--34",
+        url: "img/js_gsap_ScrollTrigger--34.png",
+        tag: ["gsap", "스크롤", "img"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--35",
+        url: "img/js_gsap_ScrollTrigger--35.png",
+        tag: ["gsap", "스크롤", "img"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--36",
+        url: "img/js_gsap_ScrollTrigger--36.png",
+        tag: ["gsap", "스크롤", "img"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--37",
+        url: "img/js_gsap_ScrollTrigger--37.png",
+        tag: ["gsap", "스크롤", "img"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--38",
+        url: "img/js_gsap_ScrollTrigger--38.png",
+        tag: ["gsap", "스크롤", "galley"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--39",
+        url: "img/js_gsap_ScrollTrigger--39.png",
+        tag: ["gsap", "가로스크롤"],
+    },
+    {
+        name: "js_gsap_ScrollTrigger--40",
+        url: "img/js_gsap_ScrollTrigger--40.png",
+        tag: ["gsap", "스크롤", "img"],
+    },
+    {
+        name: "js_gsap_slider",
+        url: "img/js_gsap_slider.png",
+        tag: ["gsap", "가로스크롤"],
+    },
+    {
+        name: "js_gsap_SplitText--01",
+        url: "img/js_gsap_SplitText--01.png",
+        tag: ["gsap", "text"],
+    },
+    {
+        name: "js_gsap_SplitText--02",
+        url: "img/js_gsap_SplitText--02.png",
+        tag: ["gsap", "text"],
+    },
+    {
+        name: "js_gsap_tab",
+        url: "img/js_gsap_tab.png",
+        tag: ["gsap", "tab"],
+    },
+    {
+        name: "js_gsap_txt effect--01",
+        url: "img/js_gsap_txt effect--01.png",
+        tag: ["gsap", "text"],
+    },
+    {
+        name: "js_gsap_txt effect--02",
+        url: "img/js_gsap_txt effect--02.png",
+        tag: ["gsap", "text"],
+    },
+    {
+        name: "js_gsap_txt effect--04",
+        url: "img/js_gsap_txt effect--04.png",
+        tag: ["gsap", "text"],
+    },
+    {
+        name: "js_gsap_txt effect--05",
+        url: "img/js_gsap_txt effect--05.png",
+        tag: ["gsap", "text", "mix"],
+    },
+    {
+        name: "js_gsap_txt effect--06",
+        url: "img/js_gsap_txt effect--06.png",
+        tag: ["gsap", "text"],
+    },
+    {
+        name: "js_gsap_나눔카드",
+        url: "img/js_gsap_나눔카드.png",
+        tag: ["gsap", "background"],
+    },
+    {
+        name: "js_gsap_무한반복 marquee--01",
+        url: "img/js_gsap_무한반복 marquee--01.png",
+        tag: ["gsap", "marquee"],
+    },
+    {
+        name: "js_gsap_무한반복 marquee--02",
+        url: "img/js_gsap_무한반복 marquee--02.png",
+        tag: ["gsap", "marquee"],
+    },
+    {
+        name: "js_gsap_무한반복 marquee--03",
+        url: "img/js_gsap_무한반복 marquee--03.png",
+        tag: ["gsap", "marquee"],
+    },
+    {
+        name: "js_hover 아래에서 글자가 올라와 밀리는 txt",
+        url: "img/js_hover 아래에서 글자가 올라와 밀리는 txt.png",
+        tag: ["text", "hover"],
+    },
+    {
+        name: "js_MacOS_btn",
+        url: "img/js_MacOS_btn.png",
+        tag: ["icon", "button", "hover"],
+    },
+    {
+        name: "js_magnetic 반전 txt",
+        url: "img/js_magnetic 반전 txt.png",
+        tag: ["text", "magnetic", "hover"],
+    },
+    {
+        name: "js_progress bar",
+        url: "img/js_progress bar.png",
+        tag: ["progress"],
+    },
+    {
+        name: "js_tabs--02",
+        url: "img/js_tabs--02.png",
+        tag: ["tab"],
+    },
+    {
+        name: "js_가로드래그 갤러리 시선이동",
+        url: "img/js_가로드래그 갤러리 시선이동.png",
+        tag: ["가로스크롤"],
+    },
+    {
+        name: "js_글자 옆에서 이미지 나옴--01",
+        url: "img/js_글자 옆에서 이미지 나옴--01.png",
+        tag: ["text", "menu"],
+    },
+    {
+        name: "js_글자 옆에서 이미지 나옴--02",
+        url: "img/js_글자 옆에서 이미지 나옴--02.png",
+        tag: ["mix", "text"],
+    },
+    {
+        name: "js_누르면 위에 메뉴 아이콘",
+        url: "img/js_누르면 위에 메뉴 아이콘.png",
+        tag: ["button", "floating"],
+    },
+];
